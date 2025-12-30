5336f0ce741884dcd1e7a36f36788d2a156e6d58
Anushree Patel <anushree.patel@icloud.com>
2025-12-31 01:51:45 +0530

    Added port

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -8,19 +8,12 @@ const PORT = process.env.PORT || 5000;
 async function start() {
   await connectDB();
 
-  app.listen(PORT, () => {
-    // eslint-disable-next-line no-console
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
 
-// Do not auto-start in tests
-if (process.env.NODE_ENV !== "test") {
-  start().catch((err) => {
-    // eslint-disable-next-line no-console
-    console.error(err);
-    process.exit(1);
-  });
-}
-
-module.exports = { start };
+start().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
